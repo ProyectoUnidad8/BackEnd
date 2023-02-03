0f55c39b0e3369be5717c6d2e8735ca60dbc232e
everyzzz <jquisper2@gmail.com>
2023-02-02 20:09:24 -0500

    feat/6 create-middleware-and-login

---
+++ prisma/migrations/20230202232344_add_cascade_delete_on_pets_users/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Pet" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "breed" TEXT,
+    "age" INTEGER NOT NULL,
+    "numberChip" TEXT,
+    "category" TEXT NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    CONSTRAINT "Pet_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Usuario" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Pet" ("age", "breed", "category", "id", "name", "numberChip", "ownerId") SELECT "age", "breed", "category", "id", "name", "numberChip", "ownerId" FROM "Pet";
+DROP TABLE "Pet";
+ALTER TABLE "new_Pet" RENAME TO "Pet";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
