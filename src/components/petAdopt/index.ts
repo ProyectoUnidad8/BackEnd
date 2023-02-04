4efe8c153069f18ee4eb40aac8514144b29d2aad
Maby <maby200@gmail.com>
2023-02-04 07:06:46 -0500

    fix: getbyID route fixed

--- src/components/petAdopt/index.ts
+++ src/components/petAdopt/index.ts
@@ -5,7 +5,7 @@ import { validateAuthorization } from "../../middleware";
 const petAdoptionRouter = Router();
 
 petAdoptionRouter.get("/", validateAuthorization, petAdoptionController.findAllPetToAdopt);
-petAdoptionRouter.get("/id", validateAuthorization, petAdoptionController.findOnePetToAdopt);
+petAdoptionRouter.get("/:id", validateAuthorization, petAdoptionController.findOnePetToAdopt);
 petAdoptionRouter.post("/", validateAuthorization, petAdoptionController.storePetToAdopt);
 petAdoptionRouter.put("/:id", validateAuthorization, petAdoptionController.updatePetToAdopt);
 petAdoptionRouter.delete("/:id", validateAuthorization, petAdoptionController.deletePetToAdopt);
