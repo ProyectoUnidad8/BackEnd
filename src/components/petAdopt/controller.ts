e1fa880901000ee4c05b0a34c4b4ba719f0c38b0
Maby <maby200@gmail.com>
2023-02-04 05:58:18 -0500

    feat: Get by id controller added

--- src/components/petAdopt/controller.ts
+++ src/components/petAdopt/controller.ts
@@ -18,3 +18,26 @@ export const findAll = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+export const findOne = async (req: Request, res: Response): Promise<void> => {
+  try {
+
+    const { id } = req.params;
+
+    const result = await prisma.petAdoption.findFirst({
+      where: {
+        id: Number(id)
+      }
+    });
+
+    res.status(200).json({
+      ok: true,
+      data: result,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: error
+    })
+  }
+};
