8d080067e3c851ca39424c2b0dd849b17294f7e1
everyzzz <jquisper2@gmail.com>
2023-02-06 11:24:14 -0500

    add id in login data and include pets in controller diagnostic

--- src/components/users/controller.ts
+++ src/components/users/controller.ts
@@ -10,10 +10,10 @@ export const findAllUsers = async (_req: Request, res: Response): Promise<void> 
 		res.status(200).json({ ok: true, data: users })
 	} catch (error) {
 		res.status(500).json({ ok: false, message: error })
-	}	
+	}
 }
 
-export const findUserPets = async (req:Request, res: Response): Promise<void> => {
+export const findUserPets = async (req: Request, res: Response): Promise<void> => {
 	try {
 		const { id } = req.params;
 		const result = await prisma.usuario.findFirst({
@@ -33,36 +33,36 @@ export const findUserPets = async (req:Request, res: Response): Promise<void> =>
 
 
 export const signup = async (req: Request, res: Response): Promise<void> => {
-	try{
+	try {
 		const { email, password, name, role } = req.body;
 
 		const hash = await bcrypt.hash(password, 13);
 
-		const result = await prisma.usuario.create({ 
+		const result = await prisma.usuario.create({
 			data: {
-				email, name, role ,password: hash
+				email, name, role, password: hash
 			}
 		});
 		res.status(201).json({ ok: true, message: "Usuario Creado", data: result })
-	} catch(error) {
+	} catch (error) {
 		res.status(500).json({ ok: false, message: error })
 	}
 }
 
 
-export const updateUser = async (req:Request, res:Response): Promise<void> => {
-	try{
+export const updateUser = async (req: Request, res: Response): Promise<void> => {
+	try {
 		const { id } = req.params;
-		const { email, password, name, role  } = req.body;
+		const { email, password, name, role } = req.body;
 
 		const hash = await bcrypt.hash(password, 13);
 
 		const result = await prisma.usuario.update({
 			where: {
-				id: Number(id) 	
+				id: Number(id)
 			},
 			data: {
-				email, name, role, password:hash
+				email, name, role, password: hash
 			}
 		});
 		res.status(200).json({ ok: true, message: "Usuario actualizado", data: result })
@@ -72,52 +72,53 @@ export const updateUser = async (req:Request, res:Response): Promise<void> => {
 }
 
 
-export const deleteUser = async (req:Request, res:Response): Promise<void> => {
+export const deleteUser = async (req: Request, res: Response): Promise<void> => {
 	try {
 		const { id } = req.params;
 		const result = await prisma.usuario.delete({
 			where: { id: Number(id) }
 		})
-		res.status(200).json({ ok:true, message: "Usuario eliminado", data: result })
+		res.status(200).json({ ok: true, message: "Usuario eliminado", data: result })
 	} catch (error) {
 		res.status(500).json({ ok: false, message: error })
 	}
 }
 
 
-export const login = async (req:Request, res: Response): Promise<void> => {
+export const login = async (req: Request, res: Response): Promise<void> => {
 	const { email, password } = req.body;
 	try {
 		if (!email || !password) {
-				res.status(401).json({ message: "Envío incorrecto de datos" });
-            return
-        }
+			res.status(401).json({ message: "Envío incorrecto de datos" });
+			return
+		}
 
-        const usuario = await prisma.usuario.findFirst({
-            where: { email },
-        })
+		const usuario = await prisma.usuario.findFirst({
+			where: { email },
+		})
 
-				const role = usuario?.role;
+		const role = usuario?.role;
+		const id = usuario?.role;
 
-        if (!usuario) {
-				res.status(401).json({ message: "Usuario equivocado" });
-          return
-        }
+		if (!usuario) {
+			res.status(401).json({ message: "Usuario equivocado" });
+			return
+		}
 
-        const isValid = await bcrypt.compare(password, usuario.password) && bcrypt.compare(email, usuario.email)
-        if (!isValid) {
-				res.status(401).json({ message: "Password incorrecto" });
-          return
-        }
+		const isValid = await bcrypt.compare(password, usuario.password) && bcrypt.compare(email, usuario.email)
+		if (!isValid) {
+			res.status(401).json({ message: "Password incorrecto" });
+			return
+		}
 
-        const token = jwt.sign(
-            { email, password }, 
-            process.env.TOKEN_SECRET as Secret,
-            {
-                expiresIn: "2h",
-            });
+		const token = jwt.sign(
+			{ email, password },
+			process.env.TOKEN_SECRET as Secret,
+			{
+				expiresIn: "2h",
+			});
 
-        res.status(201).json({ email, role, token });
+		res.status(201).json({ id, email, role, token });
 
 	} catch (error) {
 		res.status(500).json({
