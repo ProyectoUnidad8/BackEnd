4fce2187f3c47d2403b73ee26f0527168afd6788
everyzzz <jquisper2@gmail.com>
2023-02-05 23:07:33 -0500

    edit data send in login

--- src/components/users/controller.ts
+++ src/components/users/controller.ts
@@ -112,10 +112,10 @@ export const login = async (req:Request, res: Response): Promise<void> => {
             { email, password }, 
             process.env.TOKEN_SECRET as Secret,
             {
-                expiresIn: "1h",
+                expiresIn: "2h",
             });
 
-        res.status(201).json({ email, token });
+        res.status(201).json({ usuario, token });
 
 	} catch (error) {
 		res.status(500).json({
