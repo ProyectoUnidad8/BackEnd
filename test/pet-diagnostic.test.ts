0ca2ce82b87f822516c81687f31b7f33eaa9ec34
Maby <maby200@gmail.com>
2023-02-07 11:32:28 -0500

    chore: added pet-diagnostic.test.ts

---
+++ test/pet-diagnostic.test.ts
@@ -0,0 +1,45 @@
+import supertest from "supertest";
+import { app } from "../src/app";
+
+const api = supertest(app);
+
+let token = '';
+
+beforeAll(async() => {
+  const response = await api
+                            .post("/api/v1/user/login")
+                            .send({
+                              email:"test@mail.com",
+                              password:"123456"
+                            });
+  token = response.body.token;
+});
+
+
+describe('Test for /api/v1/pet-diagnostic', () => {
+
+  it('should respond a 200 OK', async () => {
+    const response = await api
+                            .get('/api/v1/pet-diagnostic')
+                            .set('Authorization', `Bearer ${token}`);
+    
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should GET a list of JSON type', async () => {
+    const response = await api
+                            .get('/api/v1/pet-diagnostic')
+                            .set('Authorization', `Bearer ${token}`);
+
+    expect(response.headers['content-type']).toMatch(/application\/json/);
+  });
+
+  it('should get an application by ID', async () => {
+    const response = await api
+                            .get('/api/v1/pet-diagnostic/1')
+                            .set('Authorization', `Bearer ${token}`);
+
+    expect(response.statusCode).toBe(200);
+
+  });
+});
